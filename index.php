82e5174cb5ae78aa6f849a45aae70d07723dbbc1
Juan <juandelsal3@gmail.com>
2018-02-18 19:58:49 +0100

    Funcion de la lupa completada y mas cambios

--- index.php
+++ index.php
@@ -6,50 +6,64 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Latest compiled and minified CSS -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="style/estilo.css">
+
 	<script type="text/javascript" src="js/main.js"></script>
 	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/js.js"></script>
 
+	<!-- Juan -->
+	<link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" 
+         rel = "stylesheet">
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<!-- Juan -->
 
+	<!-- librerias de pepe
+	<script type="text/javascript" src="js/reloj.js"></script>
+	<script type="text/javascript" src="js/countdowntimer.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,800italic,700italic,600italic,400italic,300italic,800,700,600' rel='stylesheet' type='text/css'>
+	Fin librerias -->
 </head>
 <body class="container-fluid">
 
-<?php
+	<?php
 	// Conectando, seleccionando la base de datos
-	
-	$mysqli = new mysqli('localhost', 'root', 'alumnado', 'frases_celebres');
+	// $mysqli = new mysqli('HOST', 'USER', 'PASS', 'NOMBRE_BD');
+	$mysqli = new mysqli('localhost', 'root', '', 'juego');
 	$mysqli->set_charset("utf8");
 	
 	/* En caso de que haya error... */
 	if ($mysqli->connect_errno) {
-	    echo "No se pudo conectar a la BD";
-	    echo "Error: Fallo al conectarse a MySQL debido a: \n";
-	    echo "Errno: " . $mysqli->connect_errno . "\n";
-	    echo "Error: " . $mysqli->connect_error . "\n";
-	    exit;
+		echo "No se pudo conectar a la BD";
+		echo "Error: Fallo al conectarse a MySQL debido a: \n";
+		echo "Errno: " . $mysqli->connect_errno . "\n";
+		echo "Error: " . $mysqli->connect_error . "\n";
+		exit;
 	}
 	
 	// Comenzamos las consultas
 	
-	$sql = 'select nombre_completo from autores where id_autor = 3;';
+	$sql = 'select * from cable;';
 	
 	// Si la consulta falla....
 	if(!$resultado = $mysqli->query($sql)) {
-	    echo "La consulta falló";
-	    echo "Error: La ejecución de la consulta falló debido a: \n";
-	    echo "Query: " . $sql . "\n";
-	    echo "Errno: " . $mysqli->errno . "\n";
-	    echo "Error: " . $mysqli->error . "\n";
-	    exit;
+		echo "La consulta falló ";
+		echo "Error: La ejecución de la consulta falló debido a: \n";
+		echo "Query: " . $sql . "\n";
+		echo "Errno: " . $mysqli->errno . "\n";
+		echo "Error: " . $mysqli->error . "\n";
+		exit;
 	}
 	
 	// No hay datos en esa consulta
 	if ($resultado->num_rows === 0) {
-	    echo "No hay datos contenidos.";
-	    exit;
+		echo "No hay datos contenidos.";
+		exit;
 	}
 	
 	// objeto que contiene tablas, datos, etc...
@@ -67,88 +81,93 @@
 	*/
 	$resultado->free();
 	$mysqli->close();
-?>
+	?>
 
-
+<!-- Parte Javi, Popup de inicio -->
 	<div class='popup'>
 		<div class='cnt223 text-center'>
 			<h1>Nombre del Juego</h1>
 			<h2>Descripcion</h2>
 			<p>
-			En la oficina se encuentran ubicados <strong>7 elementos</strong> que nos facilitaran el acceso a la comunicacion, responde bien a las preguntas sobre ellos en el menor tiempo posible y conseguiras  .
-			<p>
-			
-			<h2>Reglas del juego</h2>
-			<div class="imgstart">
-				<img src="images/lupa.png" alt="">
-			</div>
-			<ol class="text-left">
-				<li>Dispones de 5 minutos para:</li>
-				<li>Encontrar los 7 objetos que nos ayudaran a acceder a informacion</li>
-				<li>Utilice la Lupa para facilitar su busqueda</li>
-				<li>Una vez encontrado, ahora toca responder bien a la pregunta</li>	
-			</ol>
-			
-			<br/>
-			<h2>Introduzca su nombre</h2>
-			<input type="text" class="usuario"><br><br>
-			<button type="button" class="btn btn-primary cerrar start">START</button>
-			</p>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-10 col-xs-12">
-			<div id="popup" style="display: none;">
-    <div class="content-popup">
-        <div class="close"><a href="#" id="close"><img src="images/close.png"/></a></div>
-        <div>
-           <h2>Contenido POPUP</h2>
-           ...
-        </div>
-    </div>
-</div>
-			<div class="img" style="background-image: url('images/bg_ScapeRoom.jpg');">
-				<p>The mouse pointer position is at: <span></span></p>
-				<div class="item" id="pc"></div>
-				<div class="item" id="libro"></div>
-				<div class="item" id="movil"></div>
-				<div class="item" id="router"></div>
-				<div class="item" id="cable"></div>
-				<div class="item" id="proveedor"></div>
-				<div class="item" id="navegador"></div>
-				<div class="" id="lupa"></div>
+				En la oficina se encuentran ubicados <strong>7 elementos</strong> que nos facilitaran el acceso a la comunicacion, responde bien a las preguntas sobre ellos en el menor tiempo posible y conseguiras  .
+				<p>
+
+					<h2>Reglas del juego</h2>
+					<div class="imgstart">
+						<img src="images/lupa.png" alt="">
+					</div>
+					<ol class="text-left">
+						<li>Dispones de 5 minutos para:</li>
+						<li>Encontrar los 7 objetos que nos ayudaran a acceder a informacion</li>
+						<li>Utilice la Lupa para facilitar su busqueda</li>
+						<li>Una vez encontrado, ahora toca responder bien a la pregunta</li>	
+					</ol>
+
+					<br/>
+					<h2>Introduzca su nombre</h2>
+					<input type="text" class="usuario" id="usuario"><br><br>
+					<button type="button" class="btn btn-primary cerrar start">START</button>
+				</p>
 			</div>
 		</div>
-		<div class="col-md-2 col-xs-12">
-			<div class="img-gui" style="background-image: url('images/gui-background.jpg');">
+		<div class="row">
+			<div class="col-md-10 col-xs-12">
 
-				<div id="perfil" class="rounded">
-					<img src="images/perfil.jpg" class="rounded-circle" alt="perfil" width="100" height="100">
-					<p>PlayerName</p>
+				<div id="popup" style="display: none;">
+					<div class="content-popup">
+						<div class="close"><a href="#" id="close"><img src="images/close.png"/></a></div>
+						<div>
+							<h2>Contenido POPUP</h2>
+							...
+						</div>
+					</div>
 				</div>
+<!-- Fin Javi, Popup de inicio -->
 
-				<div id="info" class="rounded">
-					<p><strong>Has encontrado</strong></p>
-					<h3 class="display-4"><strong class="text-primary" id="itemsactuales">0</strong><strong class="text-danger">/7</strong></h3><i>items</i><hr>
-					<p><strong>Tienes</strong></p>
-					<h3 class="display-4"><strong class="text-primary" id="puntosactuales">0</strong></h3><i>puntos</i>
+				<div class="img" style="background-image: url('images/bg_ScapeRoom.jpg');">
+					<p>The mouse pointer position is at: <span></span></p>
+					<div class="item" id="pc"></div>
+					<div class="item" id="libro"></div>
+					<div class="item" id="movil"></div>
+					<div class="item" id="router"></div>
+					<div class="item" id="cable"></div>
+					<div class="item" id="proveedor"></div>
+					<div class="item" id="navegador"></div>
+					<div class="" id="lupa"></div>
 				</div>
+			</div>
 
-				<div id="vidas" class="rounded">
-					<p><strong>Vidas restantes</strong></p>
-					<img src="images/vida.png" id="vida1" class="img-responsive" alt="vida" width="50" height="45">
-					<img src="images/vida.png" id="vida2" class="img-responsive" alt="vida" width="50" height="45">
-					<img src="images/vida.png" id="vida3" class="img-responsive" alt="vida" width="50" height="45">
+
+<!-- Parte Juan, perfil usuario -->
+			<div class="col-md-2 col-xs-12">
+				<div class="img-gui" style="background-image: url('images/gui-background.jpg');">
+					<div id="perfil" class="rounded">
+						<img src="images/perfil.jpg" class="rounded-circle" alt="perfil" width="100" height="100">
+						<p class="playername">PlayerName</p>
+					</div>
+
+					<div id="info" class="rounded">
+						<p><strong>Has encontrado</strong></p>
+						<h3 class="display-4"><strong class="text-primary" id="itemsactuales">0</strong><strong class="text-danger">/7</strong></h3><i>items</i><hr>
+						<p><strong>Tienes</strong></p>
+						<h3 class="display-4"><strong class="text-primary" id="puntosactuales">0</strong></h3><i>puntos</i>
+					</div>
+
+					<div id="vidas" class="rounded">
+						<p><strong>Vidas restantes</strong></p>
+						<img src="images/vida.png" id="vida1" class="img-responsive" alt="vida" width="50" height="45">
+						<img src="images/vida.png" id="vida2" class="img-responsive" alt="vida" width="50" height="45">
+						<img src="images/vida.png" id="vida3" class="img-responsive" alt="vida" width="50" height="45">
+					</div>
+
+
+					<div id="pista" class="rounded">
+						<p><strong>Pista de la lupa</strong></p>
+						<p id="lapista">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris"</p>
+					</div>
+<!-- Fin Juan, perfil usuario -->
 				</div>
-
-
-				<div id="pista" class="rounded">
-					<p><strong>Pista de la lupa</strong></p>
-					<p id="lapista">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris"</p>
-				</div>
-
 			</div>
 		</div>
-	</div>
-</body>
-</html>
+	</body>
+	</html>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,52 @@
+/* Juan */
+
 $(document).ready(function(){
     $(document).mousemove(function(event){ 
         $("span").text("X: " + event.pageX + ", Y: " + event.pageY); 
     });
+
+
+    /* Al pulsar botón START, se guarda el nombre y activa la función de la lupa. */
+    $(".start").click(function(){
+    	var usuario = document.getElementById("usuario").value;
+    	/* Falta asegurarse de que no escribe nombre vacio */
+    	$(".playername").text(usuario);
+    	$(function() {
+            $("#lupa").draggable();
+   		});
+
+    $(".item").droppable({
+      drop: function(event, ui) {
+        $(this)
+        	.addClass("ui-state-highlight")	// Añade un color debug al div. (solo debug)
+
+        	/* Implementar aquí las pistas */
+        	//console.log(event.target.id);
+        	var item = event.target.id;
+        	if (item == "libro") {
+        		$("#lapista").text("Pista sobre libros");
+        	}
+        	if (item == "pc") {
+        		$("#lapista").text("Pista sobre pc");
+        	}
+        	if (item == "movil") {
+        		$("#lapista").text("Pista sobre movil");
+        	}
+        	if (item == "router") {
+        		$("#lapista").text("Pista sobre router");
+        	}
+        	if (item == "cable") {
+        		$("#lapista").text("Pista sobre cable");
+        	}
+        	if (item == "proveedor") {
+        		$("#lapista").text("Pista sobre proveedor");
+        	}
+        	if (item == "navegador") {
+        		$("#lapista").text("Pista sobre navegador");
+        	}
+      }
+    });
+    });
 });
+
+/* Juan */
