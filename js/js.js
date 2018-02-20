4ffd1304702ce5179f009ad376a19ac1bc112122
Geothrill <d15juan2009@gmail.com>
2018-02-20 21:40:33 +0100

    eventos para los pop-ups, reloj y clase auxiliar

--- js/js.js
+++ js/js.js
@@ -10,11 +10,54 @@ return false;
 });
 
 
- 
-
 $('.x').click(function(){
 $('.popup').hide();
 overlay.appendTo(document.body).remove();
 return false;
 });
+
+
+
+$('#pc').click(function(){
+  overlay.show();
+  overlay.appendTo(document.body);
+  $(' .popup-pc').show();
+});
+
+$('#libro').click(function(){
+  overlay.show();
+  overlay.appendTo(document.body);
+  $(' .popup-libro').show();
+});
+
+$('#movil').click(function(){
+  overlay.show();
+  overlay.appendTo(document.body);
+  $(' .popup-movil').show();
+});
+
+$('#router').click(function(){
+  overlay.show();
+  overlay.appendTo(document.body);
+  $(' .popup-router').show();
+});
+
+$('#cable').click(function(){
+  overlay.show();
+  overlay.appendTo(document.body);
+  $(' .popup-cable').show();
+});
+
+$('#proveedor').click(function(){
+  overlay.show();
+  overlay.appendTo(document.body);
+  $(' .popup-proveedor').show();
+});
+
+$('#navegador').click(function(){
+  overlay.show();
+  overlay.appendTo(document.body);
+  $(' .popup-navegador').show();
+});
+
 });
